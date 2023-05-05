2b5c1d114b4647c02ac36594d994fe1e18054bcf
FreezyCGI <patrick.fritzl@gmx.at>
2023-05-05 14:27:58 +0200

    Added Controls

--- 4AHWII_2._RacingGameTutorial/Assets/Scripts/Car.cs
+++ 4AHWII_2._RacingGameTutorial/Assets/Scripts/Car.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class Car : MonoBehaviour
 {
-
+    public float acceleration = 1;
+    public float steering = 1;
     Rigidbody rigid;
 
     // Start is called before the first frame update
@@ -17,6 +18,13 @@ public class Car : MonoBehaviour
     void Update()
     {
         float verticalInput = Input.GetAxis("Vertical");
-        rigid.AddForce(transform.forward * verticalInput * 2);
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        rigid.AddForce(transform.forward * verticalInput * acceleration * Time.deltaTime);
+        transform.Rotate(Vector3.up, horizontalInput * steering * Time.deltaTime);
+
+        Vector3 velocity = rigid.velocity;
+        velocity = transform.forward * velocity.magnitude;
+        rigid.velocity = velocity;
     }
 }
